4105928c9382d6fedd79488f2adbd262b0e535cc
Lawrence-T-Chikoore <63314782+Lawrence-T-Chikoore@users.noreply.github.com>
2023-01-31 15:38:49 +0200

    Update Weather-Observation-Station-6.sql

--- Basic Select/Weather-Observation-Station-6.sql
+++ Basic Select/Weather-Observation-Station-6.sql
@@ -8,3 +8,6 @@ city like "u%";
 
 # this works faster and better
 select distinct city from station where left(city,1) in('a','e','i','o','u')
+
+#
+select distinct CITY from Station where SUBSTRING(CITY,1,1) in ('a','e','i','o','u');
